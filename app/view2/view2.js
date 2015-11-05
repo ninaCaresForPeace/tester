cb6e87e29b845d62734f7b22354e0a38f5c4d997
Nina <nina.dr16@gmail.com>
2015-11-04 22:16:19 -0800

    Got the bootstrap carousel to animate

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,31 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute','ngAnimate','ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'CarouselDemoCtrl'//'View2Ctrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('CarouselDemoCtrl', function ($scope) {
+	  $scope.myInterval = 5000;
+	  $scope.noWrapSlides = false;
+	  var slides = $scope.slides = [];
+	  $scope.addSlide = function() {
+	    var newWidth = 600 + slides.length + 1;
+	    slides.push({
+	      image: '//placekitten.com/' + newWidth + '/300',
+	      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+	        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+	    });
+	  };
+	  for (var i=0; i<4; i++) {
+	    $scope.addSlide();
+	  }
+	});
 
-}]);
+//.controller('View2Ctrl', [function() {
+//
+//}]);
