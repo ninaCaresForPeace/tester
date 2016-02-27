fe42ae60bb398d94b9493cf71ab6e97aefafb34d
Nina <nina.dr16@gmail.com>
2016-02-27 14:26:01 -0800

    bandedNavController.js cleanup

--- app/Navigation/bandedNavController.js
+++ app/Navigation/bandedNavController.js
@@ -1,15 +1,34 @@
+/* !bandedNavController.js */
 'use strict';
-
-angular.module('myApp') 
-.controller('bandedNavControl',['$scope', '$route', 'getJsonService', function ( $scope, $route, getJsonService) {
+(function() {
+	.controller('bandedNavControl',['$scope', '$route', 'getJsonService', function ( $scope, $route, getJsonService) {
 		var jsonFile = $route.current.$$route.json;
 		getJsonService.retrieveJson(jsonFile).then(function(response){ 
-			$scope.tabs = response.data.menu;//[0].title;
+			$scope.tabs = response.data.menu;
 			$scope.dTabs = response.data.menu;
 			$scope.logo = response.data.logo;
 		});
 
-}]);
+	}]);
+})();
+
+//COMMENT OUT 2/27
+//angular.module('myApp') 
+//.controller('bandedNavControl',['$scope', '$route', 'getJsonService', function ( $scope, $route, getJsonService) {
+//		var jsonFile = $route.current.$$route.json;
+//		getJsonService.retrieveJson(jsonFile).then(function(response){ 
+//			$scope.tabs = response.data.menu;//[0].title;
+//			$scope.dTabs = response.data.menu;
+//			$scope.logo = response.data.logo;
+//		});
+//
+//}]);
+
+
+
+
+
+
 //		
 //.directive("clickTab", function () {
 //			return {
